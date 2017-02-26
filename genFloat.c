929dd7cd4cd92aa623aa8d76ce1f1c41160e907b
pipa <seceoncommons@gmail.com>
2017-02-26 09:18:05 -0500

    generate Floating point numbers

---
+++ genFloat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+# define MAX_FLOATS  100
+
+
+
+FILE* openFile(char* fileName, char* mode)
+ {
+     FILE* fp = fopen(fileName, mode);
+     if (fp == NULL)
+     {
+         perror("Error while opening the file\n");
+         exit(EXIT_FAILURE);
+     }
+     return fp;
+ }
+
+
+
+
+
+/* Generate Floating number */
+ 
+int main(int argc, char *argv[])
+{
+    // No. of Partitions of input file.
+
+    printf("Usage : -m <maxsize of floats>\n");
+    printf("argc=%d", argc); 
+    char input_file[] = "input.txt";
+    char output_file[] = "output.txt";
+
+    FILE* in = openFile(input_file, "w");
+
+    //srand(time(NULL));
+
+    // generate input
+    for (int i = 0; i < MAX_FLOATS; i++)
+    {    
+        float r = 1+(float)rand()/(float)RAND_MAX; 
+        fprintf(in, "%1f,", r);
+    }
+    fclose(in);
+
+
+    return 0;
+}
+
