fee72adcb12251d70efc03d0601d2dffa6c07141
¨JulianCaeser¨ <youremail@domain.com>
2017-02-28 02:38:42 +0530

    fixed makefile

--- heapsort/main.c
+++ heapsort/main.c
@@ -4,22 +4,22 @@
 
 int main()
 {
-	int a[] = {4,65,2,-31,0,99,2,83,782,1};
-	int n = sizeof a/sizeof a[0];
-	int i;
-	clock_t t;
-	double cpu_time_used;
+    int a[] = {4,65,2,-31,0,99,2,83,782,1};
+    int n = sizeof a/sizeof a[0];
+    int i;
+    clock_t t;
+    double cpu_time_used;
 
-	for(i=0;i<n;++i)
-		printf("%d%s",a[i],i==n-1 ? "\n" : " ");
-	
-	t = clock();
-	heapsort(a,n);
-	t = clock() - t;
-	cpu_time_used = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    for(i=0;i<n;++i)
+        printf("%d%s",a[i],i==n-1 ? "\n" : " ");
 
-	for(i=0;i<n;++i)
-		printf("%d%s",a[i],i==n-1 ? "\n" : " ");
-	printf("\nHeapsort took %f seconds to execute \n",cpu_time_used);
-	return 0;
+    t = clock();
+    heapsort(a,n);
+    t = clock() - t;
+    cpu_time_used = ((double)t)/CLOCKS_PER_SEC; // in seconds
+
+    for(i=0;i<n;++i)
+        printf("%d%s",a[i],i==n-1 ? "\n" : " ");
+    printf("\nHeapsort took %f seconds to execute \n",cpu_time_used);
+    return 0;
 }
--- heapsort/max.c
+++
@@ -1,9 +0,0 @@
-int max (int *a, int n, int i, int j, int k)
-{
-	int m = i;
-	if (j < n && a[j] > a[m])
-		m = j;
-	if (k < n && a[k] > a[m])
-		m = k;
-	return m;
-}
