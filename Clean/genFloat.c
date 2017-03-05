c0eef037c895892317196f77fdcd329b737618ff
pipa <seceoncommons@gmail.com>
2017-03-04 20:45:14 -0500

    integrated-run

--- Clean/genFloat.c
+++ Clean/genFloat.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_FLOATS 100000 // Hundred Thousand
+//#define MAX_FLOATS 100000 // Hundred Thousand
 //#define MAX_FLOATS 1000000 // One Million Floats
-//#define MAX_FLOATS 10000 // Ten Thousand
+#define MAX_FLOATS 10000 //  Ten Thousand
+
 
 //# define MAX_FLOATS 1000000000 //One Billion floats 
 
